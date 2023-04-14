c1a250f5f3c0d403b8258acd79f86516642d04c7
Rhutwik jaiswal <103762165+rhutwikjaiswal@users.noreply.github.com>
2023-04-14 15:16:29 +0530

    third

--- src/Listitem.js
+++ src/Listitem.js
@@ -5,29 +5,49 @@ import './App.css';
 const Listitem = () => {
 
     const [name, setName] = useState("");
-    const [sirname, setSirname] = useState("");
+    const [surname, setSurname] = useState("");
+    const [mobile, setMobile] = useState("");
+    const [email, setEmail] = useState("");
 
     
      const [olditems, setOldItems] = useState([]);
 
    const addItem=()=> {
-        setOldItems([...olditems,[name, sirname]]);
-        console.log(olditems);
+    if(name !== "" || surname !== "" || mobile !== "" || email !== "" ){
+      let contactInfo = "Full Name : " + name+ " "+surname+ " Mobile no : " + mobile + " Email-Id : " + email;
+      setOldItems([...olditems,contactInfo]);
+      console.log(olditems);
+    }else{
+      alert("Please fill the required input!!!");
+    }
+  
     }
 
   
     function removeItem(index) {
-      const newItems = [...olditems];
+      let confirmtext = window.confirm("Are You shure?");
+      if (confirmtext === true) {
+        let newItems = [...olditems];
       newItems.splice(index, 1);
       setOldItems(newItems);
       
+      } else {
+       console.log("You cancelled");
+      }
+     
     } 
   return (
+    <>
      <div className="container">
+      <h1>Contact Form</h1>
        <div className="addContainer">
-        First Name : <input type="text" placeholder="Enter Name" name="name" onChange={(e) =>setName(e.target.value)} />
+       <b> First Name : </b><input type="text" placeholder="Enter Name" name="name" onChange={(e) =>setName(e.target.value)} required />
          <br/>
-        Last Name : <input type="text" placeholder="Enter  Sirname" name="sirname" onChange={(e) =>setSirname(e.target.value)} />
+       <b> Last Name : </b><input type="text" placeholder="Enter Surname" name="surname" onChange={(e) =>setSurname(e.target.value)} required />
+         <br/>
+       <b> Mobile no :</b> <input type="number" placeholder="Enter Mobile" name="mobile" onChange={(e) =>setMobile(e.target.value)} required />
+         <br/>
+       <b> Email - id  : </b><input type="email" placeholder="Enter Email" name="email" onChange={(e) =>setEmail(e.target.value)} required />
          <br/>
         <button  onClick={() => addItem()}>Add</button>
       </div>
@@ -35,56 +55,16 @@ const Listitem = () => {
       {olditems.map((item, index) => (
       <div key={index} className="item">
         <span>{item}</span>
-        <button onClick={() => removeItem(index)}>Remove</button>
+      <div>
+         <button onClick={() => removeItem(index)}>Remove</button>
+        </div> 
       </div>
     ))}
        
   </div>
-
+  </>
   )
 }
 
 export default Listitem;
 
-
-// const Listitem = () => {
-
-//     const [name, setName] = useState("");
-//     const [sirname, setSirname] = useState("");
-
-    
-//      const [olditems, setOldItems] = useState([]);
-
-//    const addItem=(name)=> {
-//         setOldItems([...olditems,name]);
-//     }
-
-  
-//     function removeItem(index) {
-//       const newItems = [...olditems];
-//       newItems.splice(index, 1);
-//       setOldItems(newItems);
-//     } 
-//   return (
-//      <div className="container">
-//        <div className="addContainer">
-//         Name : <input type="text" placeholder="Enter Name" name="name" onChange={(e) =>setName(e.target.value)} />
-//         <br/>
-//        Sirname : <input type="text" placeholder="Enter  Sirname" name="sirname" onChange={(e) =>setSirname(e.target.value)} />
-//        <br/>
-//         <button  onClick={() => addItem()}>Add</button>
-//       </div>
-
-//       {olditems.map((item, index) => (
-//       <div key={index} className="item">
-//         <span>{item}</span>
-//         <button onClick={() => removeItem(index)}>Remove</button>
-//       </div>
-//     ))}
-       
-//   </div>
-
-//   )
-// }
-
-// export default Listitem;
